1fc18d56556375a1ba2269fd00cc04b7e27d760c
Denys <den.ladygin@gmail.com>
2016-06-05 13:10:45 +0300

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+package GoJava_Core4;
+
+public class Circle {
+    double r;
+    double circleArea;
+
+    {
+        circleArea = 3.14 * r * r;
+    }
+}
---
+++ area_calc.java
@@ -0,0 +1,11 @@
+package GoJava_Core4;
+
+import static GoJava_Core4.Circle.*;
+
+public class area_calc {
+    public static void main(String[] args){
+        Circle calc = new Circle();
+        Circle.r = 12;
+        System.out.println("test",);
+    }
+}
